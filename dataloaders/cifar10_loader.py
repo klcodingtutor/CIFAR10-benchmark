316b1e599ec396d392b286a49cb3798b1c48e8c2
klcodingtutor <kl.coding.tutor@gmail.com>
2025-04-02 14:13:53 +0000

    [Config]

--- dataloaders/cifar10_loader.py
+++ dataloaders/cifar10_loader.py
@@ -22,7 +22,7 @@ def get_cifar10_dataloaders(batch_size, data_dir='./data', val_split=0.1):
     
     # -----------------------------------------------------------------------------------
     # TODO:
-    SCALE_FACTOR = 1
+    SCALE_FACTOR = 0.1
     print(f"Scaling factor: {SCALE_FACTOR}")
 
     print(f"Original trainset size: {len(trainset)}")
