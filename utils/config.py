61432fe8b77c242c573a806a04df5e008faee3bd
klcodingtutor <kl.coding.tutor@gmail.com>
2025-04-02 12:30:57 +0000

    [Debug] Add print statements to display loaded configuration in config.py

--- utils/config.py
+++ utils/config.py
@@ -5,4 +5,10 @@ def load_config(config_path):
     """Load configuration from a YAML file."""
     with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
+
+    print("-----------------------------------------------------------------------------------")
+    print("Config loaded:")
+    print(config)
+    print("-----------------------------------------------------------------------------------")
+
     return config
