61d2233932b36f25b026294c79b6f552310e8a4d
klcodingtutor <kl.coding.tutor@gmail.com>
2025-04-02 11:25:32 +0000

    [Feature] Add transfer learning option to training script

--- scripts/train.py
+++ scripts/train.py
@@ -11,10 +11,13 @@ def main():
     parser.add_argument('--config', type=str, required=True, help='Path to config file')
     parser.add_argument('--dataset', type=str, default='cifar10', choices=['cifar10', 'face'], help='Dataset to use')
     parser.add_argument('--task', type=str, default=None, help='Task for face dataset (e.g., gender)')
+    parser.add_argument('--transfer_learning', default=False, help='Use transfer learning')
     args = parser.parse_args()
 
     # Load config
     config = load_config(args.config)
+    print(f"Loaded configuration from {args.config}")
+    
     config['model'] = args.model
     config['dataset'] = args.dataset
     if args.task:
@@ -33,7 +36,12 @@ def main():
         )
 
     # Setup model and logger
-    model_func = globals()[f'get_{args.model.replace("-", "_")}' + "_transfer_learning"] 
+    if args.transfer_learning:
+        model_func = globals()[f'get_{args.model.replace("-", "_")}_transfer_learning']
+        print("Used transfer learning!")
+    else:
+        model_func = globals()[f'get_{args.model.replace("-", "_")}']
+        print("Used standard model!")
     model = model_func(model_name=args.model, num_classes=10 if config['dataset'] == 'cifar10' else len(train_loader.dataset.label_to_idx))
     # print out model
     print(model)
