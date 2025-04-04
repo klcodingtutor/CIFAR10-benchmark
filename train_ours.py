8d6106d90173ffc7ad89c54e0c8b728f09d7b807
AngJianHwee <megahwee@gmail.com>
2025-04-04 02:52:31 +0000

    [Debug]

--- train_ours.py
+++ train_ours.py
@@ -60,7 +60,7 @@ with open(save_config_path, 'w') as f:
 print(f"Copied config file to {save_config_path}")
 # calculate hash
 import hashlib
-from utils.model_utils import get_actual_size
+from utils.get_actual_size import get_actual_size
 import cv2
 import numpy as np
 print(f"Hash of the config file: {hashlib.md5(open(save_config_path, 'rb').read()).hexdigest()}")
